6fef27d83dd29137864237a8feb506dd7ad37305
Roberto Cabrera <jr_cabrera_f@hotmail.com>
2015-09-13 13:35:47 -0500

    base del proyecto

---
+++ Practica_1/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: JR_Elecro
+ * Date: 13/09/2015
+ * Time: 01:34 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Practica_1
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
